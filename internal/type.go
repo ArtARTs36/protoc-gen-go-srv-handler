d232defa37c9686a33a667bc85380595867b1448
artarts36 <temicska99@mail.ru>
2024-08-14 00:54:14 +0300

    fix: fix lint

--- internal/type.go
+++ internal/type.go
@@ -5,25 +5,26 @@ import "google.golang.org/protobuf/reflect/protoreflect"
 type ValType int8
 
 const (
-	ValTypeString ValType = iota
-	ValTypeInt    ValType = iota
-	ValTypeFloat  ValType = iota
-	ValTypeBool   ValType = iota
+	ValTypeUndefined ValType = iota
+	ValTypeString    ValType = iota
+	ValTypeInt       ValType = iota
+	ValTypeFloat     ValType = iota
+	ValTypeBool      ValType = iota
 )
 
 func createValType(t protoreflect.Kind) ValType {
-	switch t {
+	switch t { //nolint: exhaustive // not need
 	case protoreflect.StringKind:
 		return ValTypeString
 	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind, protoreflect.Int64Kind,
 		protoreflect.Sint64Kind, protoreflect.Sfixed64Kind, protoreflect.Uint32Kind, protoreflect.Fixed32Kind,
 		protoreflect.Uint64Kind:
 		return ValTypeInt
-	case protoreflect.FloatKind, protoreflect.DoubleKind:
+	case protoreflect.FloatKind, protoreflect.DoubleKind, protoreflect.Fixed64Kind:
 		return ValTypeFloat
 	case protoreflect.BoolKind:
 		return ValTypeBool
 	default:
-		return ValTypeString
+		return ValTypeUndefined
 	}
 }
